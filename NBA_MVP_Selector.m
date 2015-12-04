3e91cbba96850edc90598c0b9500e72b4d3c20d2
Endika <salgueiro93@gmail.com>
2015-12-04 15:11:29 +0100

    Se otro fichero con otra temporada más.
    He comprobado que con datos reales de las últimas 4 temporadas adivina
    quien va a ser el MVP casi siempre. Sólo falla la 12-13 y porque fue
    complicado.

--- NBA_MVP_Selector.m
+++ NBA_MVP_Selector.m
@@ -4,8 +4,8 @@ clc
 
 minResMVP = 55;
 jug_ent = dlmread('jugadoresEntrenamiento.txt',' ',1,0);
-val_jug_can = dlmread('jugadoresCandidatos12-13Valores.txt',' ',1,0);
-nom_jug = readtable('jugadoresCandidatos12-13Nombres.txt');
+val_jug_can = dlmread('jugadoresCandidatos11-12Valores.txt',' ',1,0);
+nom_jug = readtable('jugadoresCandidatos11-12Nombres.txt');
 nom_jug_cell = table2cell(nom_jug);
 jug_ent_rel = zeros(size(jug_ent,1),6);
 
